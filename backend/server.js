eb4500d0ac1bbd61457f0b3a6517298b4847c837
Argiel Ducut <argiel.em@gmail.com>
2026-09-25 19:03:36 +0800

    chore: setup cors

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
-const port = 3000;
+app.use(express.json());
 
 const aiRouter = require("./routes/Ai");
 
 app.use("/api/ai", aiRouter);
+
+const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Backend is running");
